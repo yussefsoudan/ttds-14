66d0e03b7a55072f0179ea505a953722b31389af
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-02-17 12:16:59 +0000

    attempt 2 in making index faster

--- buildCollections.py
+++ buildCollections.py
@@ -11,17 +11,11 @@ from helperFunctions import findISBN
 from nltk import sent_tokenize
 from helperFunctions import getBookMetadata
 from tqdm import tqdm # progress bar 
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-from nltk.stem import *
-from nltk.stem.porter import *
-stopSet = set(stopwords.words('english'))
-stemmer = PorterStemmer()
 
+# Possible addition: author name + book name? During Author section, those might be mentioned
 bad_chars = set(['#','<','>','*','_',':','\n','@'])
 bad_words = set(['copyright','.com','www','copyediting','of fiction','e-book','all rights reserved','published by',
             'publisher','manuscript','editor','coincidental','reproduce','special thank'])
-#Possible addition: author name + book name? During Author section, those might be mentioned
 
 def buildCollections():
     client = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -36,14 +30,12 @@ def buildCollections():
 
     directory = r"/Users/yussefsoudan/Studies/Uni/year-4-cs/TTDS/CW3/playground"
     #directory = r"C:/Users/Erodotos/Desktop/Year 4/TTDS/group-project/Book3"
-    folders = ['7', 'X', 'Y', 'Z']
+    folders = ['7']
 
     for folder in folders:
         subdir = directory + '/' + folder
         for filename in tqdm(os.listdir(subdir)):
             if filename.endswith(".txt"):
-                startTime = time.time()
-                print("Started in book " + str(bookID))
 
                 filepath = subdir + '/' + filename
                 text = open(filepath, "r", encoding='utf-8').read()
@@ -65,75 +57,14 @@ def buildCollections():
                 bookID += 1
                 b = booksCollec.insert_one(bookMetadata)
                 
+                # Insert quotes
                 quotes = getQuotes(text)
-
-                
                 for quote in quotes:
-
-                    # Insert paragraph
                     quoteDoc = {"book_id" : b.inserted_id, "quote" : quote}
                     quoteDoc["_id"] = "q" + str(quoteID)
                     quoteID += 1
                     q = quotesCollec.insert_one(quoteDoc)
 
-                    # Insert inverted index entry 
-                    indexDoc = {}
-                    terms = [stemmer.stem(token.lower()) for token in re.findall(r'\w+', quote) if not token.lower() in stopSet]
-
-                    for pos, term in enumerate(terms):
-                        
-                        query = inverted_index.find({'term' : term})
-                        
-                        # Unique term
-                        if query.count() == 0: 
-                            indexDoc['_id'] = index
-                            index += 1
-                            indexDoc['term'] = term 
-                            indexDoc['term_freq'] = 1 # num of times term occurs across all quotes
-                            indexDoc['books'] = {
-                                b.inserted_id : {
-                                    'term_freq_in_book': 1, 
-                                    'quotes' : {
-                                            q.inserted_id : {'len' : len(terms), 'pos' : [pos]}
-                                        }
-                                }
-                            }
-                            inverted_index.insert_one(indexDoc)
-
-                            # Make the multi-layer indices on the inverted_index collection
-                            if index == 1:
-                                inverted_index.create_index([("term", pymongo.ASCENDING)])
-                            continue
-                        # Term already exists
-                        else:
-                            # Each term should have one entry (for now)
-                            updated = query[0]
-                            updated['term_freq'] += 1
-                            bookFound = True if b.inserted_id in updated['books'].keys() else False
-                            # Term already occured in this book
-                            if bookFound == True:
-                                updated['books'][b.inserted_id]['term_freq_in_book'] += 1 
-                                quoteFound = True if q.inserted_id in updated['books'][b.inserted_id]['quotes'].keys() else False
-                                # Term already occured in this particular quote
-                                if quoteFound == True:
-                                    updated['books'][b.inserted_id]['quotes'][q.inserted_id]['pos'].append(pos)
-                                # Term appeared in a new quote in this book
-                                else:
-                                    updated['books'][b.inserted_id]['quotes'][q.inserted_id] = {'len' : len(terms), 'pos' : [pos]}
-                                    
-                            # First time term occurs in this book
-                            else:
-                                updated['books'][b.inserted_id] = {
-                                        'term_freq_in_book': 1, 
-                                        'quotes' : {
-                                            q.inserted_id : {'len' : len(terms), 'pos' : [pos]}
-                                        }
-                                    }
-                            
-                            inverted_index.update_one({'term' : term}, {"$set" : updated})
-
-                bookTime = time.time() - startTime
-                print("Finished book " + str(bookID) + " in " + str(bookTime))
     print("Books removed for having a faulty ISBN or category: " + str(booksWithFaultyISBNOrCateg))
     client.close()
 
@@ -161,7 +92,6 @@ def getQuotes(text):
         if len(re.findall('\w+', par)) >=10 and not found_bad_chars and not found_bad_words and thank_you_count<3:
             quotes.append(par)
     return quotes
-
 
 
 buildCollections()
--- buildIndex.py
+++ buildIndex.py
@@ -2,10 +2,14 @@ import pymongo
 import nltk 
 from nltk.corpus import stopwords
 nltk.download('stopwords')
-from nltk.stem import *
-from nltk.stem.porter import *
 import re
 from tqdm import tqdm
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from nltk.stem import *
+from nltk.stem.porter import *
+stopSet = set(stopwords.words('english'))
+stemmer = PorterStemmer()
 
 stopSet = set(stopwords.words('english'))
 stemmer = PorterStemmer()
@@ -22,56 +26,60 @@ for q in tqdm(quotes):
     terms = [stemmer.stem(token.lower()) for token in re.findall(r'\w+', q['quote']) if not token.lower() in stopSet]
     for pos, term in enumerate(terms):
         query = inverted_index.find({'term' : term})
+        
         # Unique term
         if inverted_index.count_documents({'term' : term}) == 0: 
             doc['_id'] = index
             index += 1
             doc['term'] = term 
             doc['term_freq'] = 1 # num of times term occurs across all quotes
-            doc['books'] = [
-                {'_id' : q['book_id'], 
-                'term_freq_in_book': 1, 
-                'quotes' : [
-                    {'_id' : q['_id'], 'len' : len(terms), 'pos' : [pos]}
-                    ]
+            doc['books'] = {
+                q['book_id'] : { 
+                    'term_freq_in_book': 1, 
+                    'quotes' : {
+                        q['_id'] : {'len' : len(terms), 'pos' : [pos] }
+                    }
                 }
-                ]
+            }
             inverted_index.insert_one(doc)
+
+            if index == 1:
+                inverted_index.create_index([("term", pymongo.ASCENDING)])
         # Term already exists
         else:
             # Each term should have one entry (for now)
             updated = query[0]
-            updated['term_freq'] += 1
-            bookFound = False
-            for i, book in enumerate(updated['books']):
-                # Term already occured in this book
-                if book['_id'] == q['book_id']:
-                    bookFound = True
-                    updated['books'][i]['term_freq_in_book'] += 1 
-                    quoteFound = False
-                    for j, quote2 in enumerate(updated['books'][i]['quotes']):
-                        # Term already occured in this particular quote
-                        if quote2['_id'] == q['_id']:
-                            quoteFound = True
-                            updated['books'][i]['quotes'][j]['pos'].append(pos)
-                            break
-                    # Term appeared in a new quote in this book
-                    if (quoteFound == False):
-                        updated['books'][i]['quotes'].append({'_id' : q['_id'], 'len' : len(terms), 'pos' : [pos]})
-                    break
-            # First time term occurs in this book
-            if (bookFound == False):
-                updated['books'].append({
-                        '_id' : q['book_id'], 
-                        'term_freq_in_book': 1, 
-                        'quotes' : [
-                            {'_id' : q['_id'], 'len' : len(terms), 'pos' : [pos]}
-                        ]
-                    }
-                )
-            inverted_index.update_one({'term' : term}, {"$set" : updated})
+            val = updated['term_freq'] + 1
+            inverted_index.update_one({'term' : term}, {"$set" : { 'term_freq': val}})
 
-inverted_index.create_index([("term", pymongo.ASCENDING)])
-inverted_index.create_index([("books._id", pymongo.ASCENDING)])    
-inverted_index.create_index([("books.quotes._id", pymongo.ASCENDING)])       
+            # Term already occured in this book
+            if q['book_id'] in updated['books'].keys():
+                key = 'books.' + q['book_id']
+                val = updated['books'][q['book_id']]['term_freq_in_book'] + 1
+                inverted_index.update_one({'term' : term}, {"$set" : { key: val}})
+
+                # Term already occured in this particular quote
+                if q['_id'] in updated['books'][q['book_id']]['quotes'].keys():
+                    key = 'books.' + q['book_id'] + '.quotes.' + q['_id'] + '.pos' 
+                    inverted_index.update_one({'term' : term}, {"$push" : { key: pos}})
+                    
+                # Term appeared in a new quote in this book
+                else:  
+                    key = 'books.' + q['book_id'] + '.quotes.' + q['_id'] 
+                    obj = {'_id' : q['_id'], 'len' : len(terms), 'pos' : [pos]}
+                    inverted_index.update_one({'term' : term}, {"$set" : { key: obj}})
+            # First time term occurs in this book
+            else:
+                obj = {
+                        'term_freq_in_book': 1, 
+                        'quotes' : {
+                            q['_id'] : {'len' : len(terms), 'pos' : [pos]}
+                        }
+                    }
+                key = 'books.' + q['book_id']
+                inverted_index.update_one({'term' : term}, {"$set" : { key : obj}})
+
+
+# inverted_index.create_index([("books._id", pymongo.ASCENDING)])    
+# inverted_index.create_index([("books.quotes._id", pymongo.ASCENDING)])       
 client.close()
