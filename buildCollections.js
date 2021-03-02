64400a0e35824068859d81a879760c8dfb5d50de
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-03-02 15:04:02 +0000

    typo

--- buildCollections.js
+++ buildCollections.js
@@ -43,6 +43,7 @@ let buildCollections = async () => {
                     bookID += 1;
                     booksCollec.insertOne(bookMetadata, function(err, res) {
                         if (err) console.log("Error inserting book: ", err, " in book ", filename);
+                        console.log("Book ", i, " inserted out of ", files.length);
                     })
 
                     // Insert quotes 
