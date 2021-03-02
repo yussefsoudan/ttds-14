21716674350f9ac234382cc2d801babdcedc16c1
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-03-02 08:47:02 +0000

    fixing sync errors

--- buildCollections.js
+++ buildCollections.js
@@ -12,7 +12,7 @@ let buildCollections = () => {
     let folders = ['0_Other', '0', '1', '2','3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     let booksDeleted = 0;
 
-    MongoClient.connect(url, function(err, client) {
+    MongoClient.connect(url, { useUnifiedTopology : true }, function(err, client) {
 
         try {
             let booksCollec = client.db("TTDS").collection("books");
