0327c1604153bbc9b46f6dfb5c6dcea305c7cb45
Yussef Soudan <yussefsoudan@gmail.com>
2021-03-02 08:40:49 +0000

    typos

--- buildCollections.js
+++ buildCollections.js
@@ -9,7 +9,7 @@ let buildCollections = () => {
     let bookID = 0;
     let quoteID = 0;
     let directory = "../books";
-    let folders = folders = ['0_Other', '0', '1', '2','3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    let folders = ['0_Other', '0', '1', '2','3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     let booksDeleted = 0;
 
     MongoClient.connect(url, function(err, client) {
