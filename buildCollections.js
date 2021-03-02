9e3d82c3da61f74b34d12dacf11dd4c7be101131
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-03-02 14:38:17 +0000

    Only folder 1

--- buildCollections.js
+++ buildCollections.js
@@ -9,7 +9,7 @@ let buildCollections = () => {
     let bookID = 0;
     let quoteID = 0;
     let directory = "/root/books";
-    let folders = ['0_Other', '0'] // '1', '2']  ,'3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    let folders = ['1'] //, '2']  ,'3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     let booksDeleted = 0;
 
     MongoClient.connect(url, { useUnifiedTopology : true }, function(err, client) {
