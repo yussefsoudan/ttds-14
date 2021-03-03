d7a2c517b2fac798d1d883082f08a36ea2f5a8e2
Muyao Hu <humuyao@Muyaos-MacBook-Pro.local>
2021-03-03 16:55:09 +0800

    API call in react frontend

--- website/book-search-client/src/App.js
+++ website/book-search-client/src/App.js
@@ -5,24 +5,52 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
       super(props);
-      this.state = { apiResponse: "" };
+      this.state = { apiResponse: "" , quoteId: ""};
+      this.getQuoteId = this.getQuoteId.bind(this);
+      this.findQuote = this.findQuote.bind(this);
+      this.setApiResponse = this.setApiResponse.bind(this);
   }
 
-  callAPI() {
-      fetch("http://localhost:9000/testAPI")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }))
-          .catch(err => err);
+  async callAPI(route, options) {
+    let quote = await fetch(`http://localhost:9000/${route}`, options);
+    let res = await quote.json();
+    console.log("res in callAPI is: " + JSON.stringify(res));
+    return res;
   }
 
-  componentDidMount() {
-      this.callAPI();
+  getQuoteId(event) {
+    this.setState({quoteId: event.target.value});
+  }
+
+  setApiResponse(res) {
+    // alert("res: " + res);
+    console.log("res in setAPIResponse: " + JSON.stringify(res));
+    this.setState({apiResponse: JSON.stringify(res.quote)});
+  }
+
+  async findQuote(event) {
+    event.preventDefault();
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({_id: this.state.quoteId})
+    };
+    let res = await this.callAPI("findQuote", requestOptions);
+    console.log("res in findQuote: " + JSON.stringify(res));
+    this.setApiResponse(res);
+  }
+
+  async componentDidMount() {
+    let res = await this.callAPI("setUp", {});
+    console.log(res);
   }
 
   render() {
       return (
           <div>
               <SearchPage/>
+              <input type="text" value={this.state.quoteId} onChange={this.getQuoteId}></input>
+              <button onClick={this.findQuote}>Click me</button>
               <p>{this.state.apiResponse}</p>
           </div>
       );
