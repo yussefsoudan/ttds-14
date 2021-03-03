cfe66ed9fe34a7e72e6ee1e6b8f497858ca3245f
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-03-03 23:11:32 +0000

    tidying up

---
+++ index_building/py/buildIndex.py
@@ -0,0 +1,60 @@
+import pymongo 
+import nltk 
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+import re
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from nltk.stem.porter import *
+import pickle
+import gc
+
+
+def build_index(): # each term will have 200 docs max
+    stopSet = set(stopwords.words('english'))
+    stemmer = PorterStemmer()
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["TTDS"]
+    quotesCollec = db["quotes"]
+    loadSize = 1000
+    tempIndex = dict()
+    
+    for r in range(0, 57000):
+        lowerLimit = r * loadSize
+        upperLimit = lowerLimit + loadSize
+        quotes = quotesCollec.find({"_id" : {"$lt" : upperLimit, "$gte" : lowerLimit}})
+        
+        for q in quotes:
+            q_id = q['_id']
+            b_id = q['book_id']
+            terms = [stemmer.stem(token.lower()) for token in re.findall(r'\w+', q['quote']) if not token.lower() in stopSet]
+
+            for term in set(terms):
+                pos = [i for i, token in enumerate(terms) if token == term]
+                tempIndex[term] = tempIndex.get(term, {'term' : term, 'term_freq' : 0, 'books' : {}})
+                tempIndex[term]['term_freq'] += 1
+                tempIndex[term]['books'][b_id] = tempIndex[term]['books'].get(b_id, {'_id': b_id, 'term_freq_in_book': 0, 'quotes': []})
+                tempIndex[term]['books'][b_id]['term_freq_in_book'] += 1
+                tempIndex[term]['books'][b_id]['quotes'].append({'_id' : q_id, 'len' : len(terms), 'pos': pos})
+
+        if r != 0 and (r % 284 == 0):
+            print(r)
+            for value in tempIndex.values():
+                value['books'] = list(value['books'].values())
+            with open("/root/index/" + str(r-284) + "-load" + '.pickle', 'wb') as handle:
+                pickle.dump(list(tempIndex.values()), handle, protocol=pickle.HIGHEST_PROTOCOL)
+            tempIndex.clear()
+            gc.collect()
+
+    for value in tempIndex.values():
+        value['books'] = list(value['books'].values())
+    with open("/root/index/" + str(r-284) + "-load" + '.pickle', 'wb') as handle:
+        pickle.dump(list(tempIndex.values()), handle, protocol=pickle.HIGHEST_PROTOCOL)
+    tempIndex.clear()
+    gc.collect()
+
+    client.close()
+
+       
+build_index()
+     
---
+++ index_building/py/writeIndexToMongo.py
@@ -0,0 +1,26 @@
+import pymongo 
+import pickle
+import os
+
+
+def write_index():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["TTDS"]
+    inverted_index = db["invertedIndex"]
+    directory = "/root/index/"
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".pickle"): 
+            path = directory + filename 
+            if os.path.getsize(path) > 0:
+                with open(path, 'rb') as handle:
+                    docs = pickle.load(handle)
+                    if len(docs) > 3:
+                        inverted_index.insert_many(docs)
+    inverted_index.create_index([("term", pymongo.ASCENDING)])
+    inverted_index.create_index([("books._id", pymongo.ASCENDING)])
+    inverted_index.create_index([("books.quotes._id", pymongo.ASCENDING)])
+
+write_index()
+                
+
