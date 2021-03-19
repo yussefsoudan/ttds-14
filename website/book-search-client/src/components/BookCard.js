b73d7781bd0e9348fb64926ab6301a911bd38d8c
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-03-19 13:25:28 +0000

    Make cards look nice

--- website/book-search-client/src/components/BookCard.js
+++ website/book-search-client/src/components/BookCard.js
@@ -1,82 +1,73 @@
 import React from 'react';
-
-
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
 
 const useStyles = makeStyles((theme) => ({
-    cardGrid: {
-      paddingTop: theme.spacing(2),
-      paddingBottom: theme.spacing(2),
-    },
-    card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-      flexGrow: 1,
-    },
-
-  }));
-  
+  root: {
+    display: 'flex',
+    marginTop: '5%',
+    marginBottom: '5%',
+  },
+  cover: {
+    maxWidth: '100%',
+    padding: '0.7em'
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+  },
+}));
 
 export default function BookCard({resultObj}) {
-    // console.log("Book card",resultObj)
     const classes = useStyles();
     let book  = resultObj;
-    let quote = resultObj.quote ?  resultObj.quote : "" //resultObj.quote.quoteStr;
-    // console.log(book)
-    // console.log(quote)
+    let quote = resultObj.quote ?  resultObj.quote : "";
+    let authors = ""
+    for (let i = 0; i < book.authors.length; i++) {
+      if (i == book.authors.length - 1) {
+        authors += book.authors[i]
+      } else {
+        authors += book.authors[i] + ", "
+      }
+    }
 
     return (
-    <Grid 
-    item key={book._id}  
-    xs={12} 
-    sm={6} 
-    className={classes.cardGrid}
-    
-    // md={4}
-    >
-        <Card raised className={classes.card}>
+
+      <Card className={classes.root} >
         <CardMedia
-            className={classes.cardMedia}
-            // image="https://source.unsplash.com/random"
-            image = {book.thumbnail}
-            title={book.title}
-        />
-        <CardContent className={classes.cardContent}>
-            <Typography gutterBottom variant="h5" component="h2">
-            {book.title}
-            </Typography>
-            {book.authors.map((author)=>
-            <Typography component="h3">
-             {author}
-            </Typography>)}
-            
-            <Typography component="h4">
-            {quote}
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="small" color="primary" href={book.previewLink}>
-            View
-            </Button>
-            <Button size="small" color="primary">
-            Edit
-            </Button>
-        </CardActions>
-        </Card>
-      </Grid>
+          className={classes.cover}
+        >
+          <img src={book.thumbnail} style={{"max-height" : "100%", "max-width" : "100%", 
+          "border" : "0.2em solid black", "border-radius" : "0.1em"}}/>
+        </CardMedia>
+        <div className={classes.details}>
+          <CardContent className={classes.content}>
+            <Typography variant="h5" component="h2">
+              {book.title}
+              </Typography>
+              <Typography gutterBottom component="h3"   >
+              {authors}
+              </Typography>
+              
+              <Typography gutterBottom component="h4" variant="h6" style={{"color" : "darkblue", "font-style": "italic"}} >
+              <q cite="https://www.mozilla.org/en-US/about/history/details/">{quote}</q>
+              </Typography>
+                <Typography size="small" color="primary" href={book.previewLink}  target="_blank" style={{'textTransform': 'none'}}>
+                View on Google Books
+                </Typography>
+          </CardContent>
+        </div>
+      </Card>
     )              
-  }
+}
+
