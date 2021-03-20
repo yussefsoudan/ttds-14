f1c5b8ac5da28349f32e1f880ccc7cfbac771d66
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-03-20 22:26:30 +0000

    adding more logging

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from ranking import *
 # from api.utils.cache import ResultsCache
 # from query_completion.model import predict_next_word
 
-app = Flask(__name__, static_url_path = '', static_folder='website/book-search-client/build')
+app = Flask(__name__, static_url_path = '', static_folder = 'website/book-search-client/build')
 CORS(app)
 
 # SWAGGER_URL = '/api/docs'
@@ -51,7 +51,8 @@ def merge_dict_lists (l1,l2,key):
 @app.route('/')
 def serve():
     print("SERVING....")
-    return send_from_directory(app.static_folder, 'index.html')
+    print(str(app.static_folder))
+    return send_from_directory(app.static_folder,'index.html')
 
 @app.route('/quote_from_id', methods=['POST'])
 def get_quote_from_quote_id():
