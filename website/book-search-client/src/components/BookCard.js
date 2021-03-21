baf7baf8ea9354e76992372e8bfdcce55bb5ab5b
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-03-21 13:17:10 +0000

    Card shape adapts to whether it's a quote or a book plus stars

--- website/book-search-client/src/components/BookCard.js
+++ website/book-search-client/src/components/BookCard.js
@@ -14,6 +14,7 @@ import Collapse from '@material-ui/core/Collapse';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Highlighter from "react-highlight-words";
 import ShowMoreText from 'react-show-more-text';
+import Stars from 'react-stars-display';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,6 +32,7 @@ const useStyles = makeStyles((theme) => ({
   details: {
     display: 'flex',
     flexDirection: 'column',
+    minWidth: '75%'
   },
   content: {
   },
@@ -61,10 +63,69 @@ export default function BookCard({resultObj, searchTerms}) {
         authors += book.authors[i] + ", "
       }
     }
+    let bookDetails = <CardContent style={{"fontStyle" : "italic"}}>
+                        <Typography gutterBottom variant="h6" component="h2"  >
+                        ––– Book categories: {book.categories}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="h2"  >
+                        ––– Number of pages: {book.pageCount}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="h2"   >
+                        ––– Published date: {book.publishedDate}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="h2"   >
+                        ––– ISBN: {book['isbn-13']}
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="h2"   >
+                        ––– Average rating: <Stars
+                                          stars={book.averageRating}
+                                          size={15} //optional
+                                          spacing={2} //optional
+                                          fill='#ea9c46' //optional
+                                          />
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="h2"   >
+                        ––– Number of ratings: {book.ratingsCount}
+                        </Typography>
+                      </CardContent>
+      let displayedQuote = bookDetails 
+      if (quote.length > 300) {
+        displayedQuote = <ShowMoreText
+                          /* Default options */
+                          lines={3}
+                          more='Show more'
+                          less='Show less'
+                          className='content-css'
+                          anchorClass='my-anchor-css-class'
+                          expanded={false}
+                          width={0}
+                          >
+                            <q cite="https://www.mozilla.org/en-US/about/history/details/">{
+                          <Highlighter
+                          highlightClassName="YourHighlightClass"
+                          searchWords={searchTerms}
+                          autoEscape={true}
+                          textToHighlight={quote}
+                        >
+                          
+                          </Highlighter>}</q>
+                          </ShowMoreText>
+      } else if (quote.length <= 300 && quote.length > 0) {
+        displayedQuote = <q cite="https://www.mozilla.org/en-US/about/history/details/">{
+                          <Highlighter
+                          highlightClassName="YourHighlightClass"
+                          searchWords={searchTerms}
+                          autoEscape={true}
+                          textToHighlight={quote}
+                          >
+                          </Highlighter>}</q>
+      }
 
     const handleExpandClick = () => {
       setExpanded(!expanded);
     };
+
+  
 
 
     return (
@@ -78,49 +139,17 @@ export default function BookCard({resultObj, searchTerms}) {
         </CardMedia>
         <div className={classes.details}>
         
-          <CardContent className={classes.content}>
+          <CardContent className={classes.content} >
             <Typography variant="h5" component="h2">
               {book.title}
             </Typography>
-            <Typography gutterBottom component="h3"   >
+            <Typography gutterBottom component="h3" >
               {authors}
             </Typography>
               
             <Typography gutterBottom component="h4" variant="h6" style={{"color" : "darkblue", "font-style": "italic"}} >
               
-            {(quote.length > 300 ? 
-            <ShowMoreText
-            /* Default options */
-            lines={3}
-            more='Show more'
-            less='Show less'
-            className='content-css'
-            anchorClass='my-anchor-css-class'
-            expanded={false}
-            width={0}
-            >
-              <q cite="https://www.mozilla.org/en-US/about/history/details/">{
-            <Highlighter
-            highlightClassName="YourHighlightClass"
-            searchWords={searchTerms}
-            autoEscape={true}
-            textToHighlight={quote}
-          >
-            
-            </Highlighter>}</q>
-            </ShowMoreText>
-            : 
-            <q cite="https://www.mozilla.org/en-US/about/history/details/">{
-            <Highlighter
-            highlightClassName="YourHighlightClass"
-            searchWords={searchTerms}
-            autoEscape={true}
-            textToHighlight={quote}
-            >
-            </Highlighter>}</q>
-            )}
-            
-                
+            {displayedQuote}
               
             </Typography>
             <Button disableRipple size="small" color="primary" href={book.previewLink}  target="_blank" style={{'cursor' : 'pointer',  
@@ -130,46 +159,30 @@ export default function BookCard({resultObj, searchTerms}) {
             <hr  style={{
               color: 'lightblue',
               align: 'left',
-              margin: 0,
               width: '100%'
             }}/>
           </CardContent>
+          {(quote.length > 0) ? <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+          </IconButton> : ""}
           
-          <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Typography gutterBottom variant="h6" component="h2" >
-                Further Book Details
-              </Typography>
-              <Typography gutterBottom component="h3"   >
-              • Book categories: {book.categories}
-              </Typography>
-              <Typography gutterBottom component="h3"   >
-              • Number of pages: {book.pageCount}
-              </Typography>
-              <Typography gutterBottom component="h3"   >
-              • Published date: {book.publishedDate}
-              </Typography>
-              <Typography gutterBottom component="h3"   >
-              • ISBN: {book['isbn-13']}
-              </Typography>
-              <Typography gutterBottom component="h3"   >
-              • Average rating: {book.averageRating == "" ? "" :  book.averageRating + "/5"}
-              </Typography>
-              <Typography gutterBottom component="h3"   >
-              • Number of ratings: {book.ratingsCount}
-              </Typography>
-            </CardContent>
-          </Collapse>
+          {(quote.length > 0) ? 
+            
+            
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              {(quote.length > 0) ? bookDetails : ""}
+            </Collapse>
+             :
+            ""
+          }
+          
         </div>
       </Card>
 
