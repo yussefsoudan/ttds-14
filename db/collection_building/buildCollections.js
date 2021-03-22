32cda5ea9df0ea7de330e9589748a230aabc4030
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-03-22 22:59:02 +0000

    bug fixing before building collections

--- db/collection_building/buildCollections.js
+++ db/collection_building/buildCollections.js
@@ -10,7 +10,7 @@ let buildCollections = async () => {
     let bookID = 0;
     let quoteID = 0;
     let directory = "/root/books";
-    let folders = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    let folders = ['0_Other','0','1','2','3','4','5','6','7','8','9','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     let booksDeleted = 0;
     let text = await fs.readFile('./nltk_stop_words.txt', 'utf8'); 
     let stopWords = new Set(text.split("\n"));
