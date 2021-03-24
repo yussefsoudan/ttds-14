20bfcb1057bd8f716335cc7f311a8fcbf65e8639
Yussef Soudan <yussefsoudan@gmail.com>
2021-03-24 14:56:59 +0000

    making the droplet the server'

--- MongoDB.py
+++ MongoDB.py
@@ -11,7 +11,7 @@ class MongoDB:
         DB_PASS='thenittygrittyofELnitty'
         DB_USER='readerTTDS'
         DB_NAME='TTDS' 
-        DB_HOST='188.166.173.191'
+        DB_HOST='localhost'
         PORT = 27017
         client = MongoClient(f'mongodb://{DB_USER}:{DB_PASS}@{DB_HOST}:{PORT}') 
         db = client[DB_NAME]
