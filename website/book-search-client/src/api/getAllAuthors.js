6cf0dd0a3d330067ade97056019da7767cec9512
Yussef Soudan <yussefsoudan@Yussefs-MBP.broadband>
2021-03-24 16:54:30 +0000

    changing url to localhost

--- website/book-search-client/src/api/getAllAuthors.js
+++ website/book-search-client/src/api/getAllAuthors.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 // const findQuoteURL = 'http://localhost:9000/findQuote';
-const getAuthorsURL = 'http://127.0.0.1:5000/get_all_authors';
+const getAuthorsURL = 'http://localhost:5000/get_all_authors';
 
 const getAllAuthors = async() => {
     return axios.post(getAuthorsURL,{
