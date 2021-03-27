5e826f5a0df6274a88b9fadca05c7c7488bbc713
Muyao Hu <humuyao@Muyaos-MacBook-Pro.local>
2021-03-27 22:52:20 +0800

    added trimming to query for quote search

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def get_quotes_from_terms():
     
     # quote contains character " twice and they are at the beginning / end of the quote => phrase search is true
     phrase_search = False
-    char_pos = [char.start() for char in re.finditer('"', details['quote'])]
+    char_pos = [char.start() for char in re.finditer('"', details['quote'].strip())]
     if len(char_pos) == 2:
         if char_pos[0] == 0 and char_pos[1] == len(details['quote']) - 1:
             phrase_search = True
